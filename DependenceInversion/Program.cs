c7ff3445e6b3ea3e0932ec3986b2cbb004241301
Quang Viet, Nguyen <nguye374@sheridancollege.ca>
2021-07-13 16:40:15 -0400

    Add project files.

---
+++ DependenceInversion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DependenceInversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
